811147c06a80dbd2a900b7ffdc7c21d0660bd5a0
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2022-04-27 23:11:27 +0100

    implemented basic customer validator

--- App/Services/CustomerService.cs
+++ App/Services/CustomerService.cs
@@ -2,22 +2,23 @@
 using App.Data;
 using App.Data.Repositories;
 using App.Model;
+using App.Validators;
 
 namespace App.Services
 {
     public class CustomerService
     {
-        public bool AddCustomer(string firname, string surname, string email, DateTime dateOfBirth, int companyId)
-        {
-            if (string.IsNullOrEmpty(firname) || string.IsNullOrEmpty(surname))
-            {
-                return false;
-            }
+        private ICustomerValidator _customerValidator;
 
-            if (!email.Contains("@") && !email.Contains("."))
-            {
+        public CustomerService(ICustomerValidator customerValidator)
+        {
+            _customerValidator = customerValidator;
+        }
+
+        public bool AddCustomer(string firstName, string surname, string email, DateTime dateOfBirth, int companyId)
+        {
+            if (!_customerValidator.ValidateNames(firstName, surname) || !_customerValidator.ValidateEmail(email))
                 return false;
-            }
 
             var now = DateTime.Now;
             int age = now.Year - dateOfBirth.Year;
@@ -36,7 +37,7 @@ namespace App.Services
                                    Company = company,
                                    DateOfBirth = dateOfBirth,
                                    EmailAddress = email,
-                                   Firstname = firname,
+                                   Firstname = firstName,
                                    Surname = surname
                                };
 
---
+++ App/Validators/CustomerValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace App.Validators
+{
+    public interface ICustomerValidator
+    {
+        bool ValidateNames(params string[] names);
+
+        bool ValidateEmail(string email);
+    }
+
+    public class CustomerValidator
+    {
+        public bool ValidateNames(params string[] names)
+        {
+            if (names is null)
+                throw new ArgumentNullException(nameof(names));
+            
+            if (names.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(names));
+            
+            foreach (var name in names)
+            {
+                if (!NameIsValid(name))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidateEmail(string email) => !string.IsNullOrWhiteSpace(email) &&
+                                                   email.Contains("@") &&
+                                                   email.Contains(".");
+        private bool NameIsValid(string name) => !string.IsNullOrWhiteSpace(name);
+    }
+}

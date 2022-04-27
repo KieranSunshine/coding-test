ccd911e77d264ca589e2265c30874c7ffd63b9cc
Kieran Sunshine <kieransunshine@hotmail.co.uk>
2022-04-27 23:45:08 +0100

    Added interface comments and ensured interface is implemented

--- App/Validators/CustomerValidator.cs
+++ App/Validators/CustomerValidator.cs
@@ -4,12 +4,22 @@ namespace App.Validators
 {
     public interface ICustomerValidator
     {
+        /// <summary>
+        /// Tests each name in the array to ensure that it meets the acceptable criteria.
+        /// </summary>
+        /// <param name="names">The names to test.</param>
+        /// <returns>Returns true when all names are valid, otherwise false.</returns>
         bool ValidateNames(params string[] names);
 
+        /// <summary>
+        /// Tests the provided string to determine whether it is a valid email address.
+        /// </summary>
+        /// <param name="email">The string to test.</param>
+        /// <returns>Returns true if the provided string is a valid email address, otherwise false.</returns>
         bool ValidateEmail(string email);
     }
 
-    public class CustomerValidator
+    public class CustomerValidator : ICustomerValidator
     {
         public bool ValidateNames(params string[] names)
         {
